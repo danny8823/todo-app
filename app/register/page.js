5f9abd8c4f2395ab1f20a583ddf1532b754c78db
danny8823 <dannyyoo714@gmail.com>
2024-10-04 15:42:37 -0700

    Added a form

--- app/register/page.js
+++ app/register/page.js
@@ -1,8 +1,39 @@
 
 export default function Register(){
     return (
-        <div>
-            Register
+        <div class = 'bg-red-100 h-svh flex justify-center items-center'>
+            <form class = 'bg-blue-300 flex flex-col w-3/6 p-3 rounded-lg shadow-2xl'>
+                <h1 class = 'text-center text-4xl'>Register</h1>
+                <label
+                    class = 'mb-2 text-lg'>
+                    Username:
+                </label>
+                <input
+                    class = 'w-full mb-2 text-2xl'
+                    placeholder = 'username'
+                />
+                <label
+                    class = 'mb-2 text-lg'>
+                    Email:
+                </label>
+                <input
+                    class = 'w-full mb-2 text-2xl'
+                    placeholder = 'email'
+                />
+                <label
+                    class = 'mb-2 text-lg'>Password:</label>
+                <input
+                    class = 'w-full mb-2 text-2xl'
+                    placeholder = 'password'
+                />
+                <label
+                    class = 'mb-2 text-lg'>Confirm Password:</label>
+                <input
+                    class = 'w-full mb-2 text-2xl'
+                    placeholder = 'confirm-password'
+                />
+                <button>Register</button>
+            </form>
         </div>
     )
 }
