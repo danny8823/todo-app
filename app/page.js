389adaa85f837fa6d1bfc19d0617a563ddf23ce1
danny8823 <dannyyoo714@gmail.com>
2024-10-06 09:55:50 -0700

    started login form on homepage

--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
-
-
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>ToDo Appity app</h1>
+      <form class = 'bg-blue-100 w-96 h-96 p-4'>
+        <label>Username:</label>
+        <input
+          class = 'w-80'
+          />
+        <label>Password:</label>
+        <input
+          class = 'w-80'/>
+      </form>
     </div>
   );
 }
