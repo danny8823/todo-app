4a9ca05b39aa3d8659d8258c152fe3cf1ddae262
danny8823 <dannyyoo714@gmail.com>
2024-10-07 11:09:41 -0700

    Finished login form design

--- app/page.js
+++ app/page.js
@@ -1,19 +1,37 @@
+'use client'
+
+import Link from "next/link";
+
 export default function Home() {
+  
+  const loginButtonHandler = () => {
+    console.log('hi')
+  }
+
   return (
-    <div>
-      <h1>ToDo Appity app</h1>
-      <form class = 'bg-blue-100 w-96 h-96 p-4'>
-        <label>Username:</label>
-        <input
-          class = 'w-80'
-          />
-        <label>Password:</label>
-        <input
-          class = 'w-80'/>
-        <button 
-          class = 'bg-black text-white mt-5 w-80 p-2'  
-        >Login</button>
-      </form>
+    <div className = 'flex flex-col items-center justify-center w-full h-screen bg-slate-100'>
+      <h1 className = 'mb-10 text-3xl'>To-Do Appity App</h1>
+      <div>
+        <form className = 'bg-blue-100 w-96 h-96 p-4 rounded-2xl shadow-xl'>
+          <label className = 'text-2xl'>Username:</label>
+          <input
+            className = 'w-full mb-3 text-xl p-1 rounded-lg'
+            placeholder = 'username'
+            />
+          <label className = 'text-2xl'>Password:</label>
+          <input
+            className = 'w-full text-xl p-1 rounded-lg'
+            placeholder = 'password'
+            />
+          <button
+            onClick = {loginButtonHandler}
+            className = 'bg-black text-white mt-5 w-full p-2 rounded-full text-2xl'  
+          >Login</button>
+        </form>
+      </div>
+      <div>
+        <span><Link className = 'text-blue-500' href = '/register'>Don't have an account? Register</Link></span>
+      </div>
     </div>
   );
 }
--- app/register/page.js
+++ app/register/page.js
@@ -3,33 +3,33 @@ export default function Register(){
     return (
         <div class = 'bg-red-100 h-svh flex justify-center items-center'>
             <form class = 'bg-blue-300 flex flex-col w-3/6 p-3 rounded-lg shadow-2xl'>
-                <h1 class = 'text-center text-4xl'>Register</h1>
+                <h1 className = 'text-center text-4xl'>Register</h1>
                 <label
-                    class = 'mb-2 text-lg'>
+                    className = 'mb-2 text-lg'>
                     Username:
                 </label>
                 <input
-                    class = 'w-full mb-2 text-2xl'
+                    className = 'w-full mb-2 text-2xl'
                     placeholder = 'username'
                 />
                 <label
-                    class = 'mb-2 text-lg'>
+                    className = 'mb-2 text-lg'>
                     Email:
                 </label>
                 <input
-                    class = 'w-full mb-2 text-2xl'
+                    className = 'w-full mb-2 text-2xl'
                     placeholder = 'email'
                 />
                 <label
-                    class = 'mb-2 text-lg'>Password:</label>
+                    className = 'mb-2 text-lg'>Password:</label>
                 <input
                     class = 'w-full mb-2 text-2xl'
                     placeholder = 'password'
                 />
                 <label
-                    class = 'mb-2 text-lg'>Confirm Password:</label>
+                    className = 'mb-2 text-lg'>Confirm Password:</label>
                 <input
-                    class = 'w-full mb-2 text-2xl'
+                    className = 'w-full mb-2 text-2xl'
                     placeholder = 'confirm-password'
                 />
                 <button>Register</button>
